840c88f4022aa1ffd375a3c93e26a85ee0fa5f47
GuiReis1303 <91472369+GuiReis1303@users.noreply.github.com>
2022-05-12 17:06:02 -0300

    aa
    aa

--- src/Formulario.js
+++ src/Formulario.js
@@ -5,7 +5,7 @@ const Formulario = () => {
             <h1> Cadastro </h1>
             <input placeholder="Seu nome..." />
             <br/>
-            <input placeholder="Sua senha..." />
+            <input placeholder="Sua senha..." />aaaaaaa
             <br/>
             <button> Salvar </button>
 
